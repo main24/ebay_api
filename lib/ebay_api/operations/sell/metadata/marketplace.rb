4142885225656572de1f321a0d2d86e1fda9c585
Ulan Djamanbalaev <ulandja@amazon.com>
2026-02-12 12:08:37 +0000

    Add `sell.metadata.marketplace.get_classified_ad_policies` endpoint

--- lib/ebay_api/operations/sell/metadata/marketplace.rb
+++ lib/ebay_api/operations/sell/metadata/marketplace.rb
@@ -7,6 +7,7 @@ class EbayAPI
 
         require_relative "marketplace/get_item_condition_policies"
         require_relative "marketplace/get_category_policies"
+        require_relative "marketplace/get_classified_ad_policies"
       end
     end
   end
