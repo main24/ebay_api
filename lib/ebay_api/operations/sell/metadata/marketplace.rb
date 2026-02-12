d4b1138f117d93e4c96b9b1aede4e7fd45be3669
Ulan Djamanbalaev <ulandja@amazon.com>
2026-02-12 12:13:09 +0000

    Add `sell.metadata.marketplace.get_listing_type_policies` endpoint

--- lib/ebay_api/operations/sell/metadata/marketplace.rb
+++ lib/ebay_api/operations/sell/metadata/marketplace.rb
@@ -10,6 +10,7 @@ class EbayAPI
         require_relative "marketplace/get_classified_ad_policies"
         require_relative "marketplace/get_automotive_parts_compatibility_policies"
         require_relative "marketplace/get_listing_structure_policies"
+        require_relative "marketplace/get_listing_type_policies"
       end
     end
   end
